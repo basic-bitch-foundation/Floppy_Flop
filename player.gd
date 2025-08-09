c31a9a475c359e74ede9f8c56692ed5147152d58
basic-bitch-foundation <ytpartnership2427@gmail.com>
2025-08-09 23:55:20 +0530

    New logic for tiles detection
    -Added new tileset for red tiles or score loss with unitary time .
    -change logic for collectible use custom data layers

--- player.gd
+++ player.gd
@@ -4,49 +4,77 @@ const SPEED = 100
 const JUMP_FORCE = -300
 const GRAVITY = 600
 
-var score = 0
-@onready var tilemap = get_parent().get_node("TileMap/Layer0") 
+var score: int = 0
+
+var on_bad_tile_area: bool = false
+var bad_tile_area_timer: float = 0.0
+const BAD_TILE_DAMAGE_INTERVAL := 1.0 
+
+@onready var tilemap_layer0: TileMapLayer = get_tree().get_current_scene().get_node("TileMap/Layer0")
+@onready var bad_tile_detector = $BadTileDetector
+
+func _ready() -> void:
+	bad_tile_detector.connect("body_entered", Callable(self, "_on_bad_tile_entered"))
+	bad_tile_detector.connect("body_exited", Callable(self, "_on_bad_tile_exited"))
+
 func _physics_process(delta: float) -> void:
-	# Gravity
+	
 	if not is_on_floor():
 		velocity.y += GRAVITY * delta
 	else:
 		velocity.y = 0
 
-	# Horizontal movement
 	velocity.x = 0
 	if Input.is_action_pressed("move_right"):
 		velocity.x += SPEED
 	if Input.is_action_pressed("move_left"):
 		velocity.x -= SPEED
 
-	# Jump
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_FORCE
 
-	# Apply movement
 	move_and_slide()
 
-	# Check for collectibles under/around player
-	_check_collectibles()
+	
+	_check_collectibles(delta)
 
-func _check_collectibles() -> void:
-	if tilemap == null:
+	
+	if on_bad_tile_area:
+		bad_tile_area_timer += delta
+		if bad_tile_area_timer >= BAD_TILE_DAMAGE_INTERVAL:
+			score -= 1
+			bad_tile_area_timer = 0.0
+			print("Ouch! On bad tile area! Score:", score)
+	else:
+		bad_tile_area_timer = 0.0
+
+func _check_collectibles(_delta: float) -> void:
+	if tilemap_layer0 == null:
 		return
 
-	# Player collision box in tilemap coordinates
-	var half_size = Vector2(8, 8) # adjust to your tile/player size
-	var min_tile = tilemap.local_to_map(global_position - half_size)
-	var max_tile = tilemap.local_to_map(global_position + half_size)
+	var detect_points = [
+		Vector2(-4, 6),
+		Vector2(0, 6),
+		Vector2(4, 6),
+	]
 
-	# Loop through all overlapped tiles
-	for tx in range(int(min_tile.x), int(max_tile.x) + 1):
-		for ty in range(int(min_tile.y), int(max_tile.y) + 1):
-			var coords = Vector2i(tx, ty)
-			var tile_data = tilemap.get_cell_tile_data(coords)
-			if tile_data and tile_data.has_custom_data("type"):
-				var t = tile_data.get_custom_data("type")
-				if t == "collectible":
-					score += 1
-					tilemap.erase_cell(coords)
-					print("Collected! Score: ", score)
+	for offset in detect_points:
+		var detect_pos = tilemap_layer0.to_local(global_position + offset)
+		var tile_coords: Vector2i = tilemap_layer0.local_to_map(detect_pos)
+		var tile_data: TileData = tilemap_layer0.get_cell_tile_data(tile_coords)
+
+		if tile_data and tile_data.has_custom_data("type"):
+			var t = tile_data.get_custom_data("type")
+
+			if t == "collectible":
+				score += 1
+				tilemap_layer0.set_cell(tile_coords, -1)
+				print("Collected! Score:", score)
+
+func _on_bad_tile_entered(_body: Node) -> void:
+	on_bad_tile_area = true
+	print("Entered bad tile area")
+
+func _on_bad_tile_exited(_body: Node) -> void:
+	on_bad_tile_area = false
+	print("Exited bad tile area")
